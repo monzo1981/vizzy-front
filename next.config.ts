7d1120e77b411a05057f157765544e02f680d5a5
Braa355 <braa_355@hotmail.com>
2025-10-01 18:44:46 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true, // لحل مشاكل الصور في الـ development
     domains: [], // لسنا نحتاجها مع الـ img tag العادي
